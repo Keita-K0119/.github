01ab86a9a96b9450229160820fc985f23a2b7065
Keita-K0119 <70999922+Keita-K0119@users.noreply.github.com>
2021-02-26 17:02:41 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+  window.onload = function() {
+    const spin = document.getElementById('loading');
+    spin.classList.add('loaded');
+  }
+
+    const open = document.getElementById('open');
+    const overlay = document.querySelector('.overlay');
+    const close = document.getElementById('close');
+  
+    open.addEventListener('click', () => {
+      overlay.classList.add('show');
+      open.classList.add('hide');
+    });
+  
+    close.addEventListener('click', () => {
+      overlay.classList.remove('show');
+      open.classList.remove('hide');
+    });
+    
+
+}
